38385cb3326c33220108629c181a3c7ea80ae02b
Nicolás Yagüe <nyaguenasif@gmail.com>
2023-09-05 16:22:42 -0300

    responsive

--- app/page.js
+++ app/page.js
@@ -8,7 +8,7 @@ export default function Home() {
   return (
     <main className="min-h-screen w-full">
       <header
-        className={`w-full h-screen items-center justify-center  flex flex-col bg-[url(../src/assets/img/background/6B99E25D-CE04-4971-B271-2EA71408B544.jpg)]  bg-cover bg-center bg-fixed`}
+        className={`w-full h-screen items-center justify-center  flex flex-col bg-[url(../src/assets/img/background/6B99E25D-CE04-4971-B271-2EA71408B544.jpg)]  bg-cover bg-center md:bg-fixed`}
       >
         <div className="absolute w-full h-screen bg-black opacity-50 bg-fixed"></div>
         <h1 className="mt-20 font-cinzel z-10 text-4xl md:text-7xl text-transparent bg-clip-text bg-gradient-to-r from-winter-600 via-winter-100 to-winter-300 tracking-widest font-extrabold border border-winter-600 p-5">
@@ -19,18 +19,18 @@ export default function Home() {
         </p>
         <a
           href="#"
-          className="bg-winter-700 text-xl z-10 px-4 py-3 hover:shadow-2xl hover:shadow-winter-200 transition-all rounded-md flex items-center justify-center font-cinzel font-medium tracking-wider text-winter-300 mt-10"
+          className="bg-winter-600 text-xl z-10 px-4 py-3 hover:shadow-2xl hover:shadow-winter-200 transition-all rounded-md flex items-center justify-center font-cinzel font-medium tracking-wider text-winter-700 mt-10"
         >
           EXPLORAR
         </a>
       </header>
 
       <section
-        className={`w-full h-screen  flex bg-[url(../src/assets/img/background/DACA1378-472C-48FE-B0C2-EC65639013D3.jpg)]  bg-cover bg-center  bg-fixed`}
+        className={`w-full h-screen  flex bg-[url(../src/assets/img/background/DACA1378-472C-48FE-B0C2-EC65639013D3.jpg)] bg-cover bg-center  md:bg-fixed`}
       >
         <div className="absolute w-full h-screen bg-black bg-opacity-50 bg-fixed"></div>
         <div className="z-10 w-full md:w-1/2  p-20  ">
-          <div className="w-full h-full flex flex-col gap-10 border border-winter-400 p-10">
+          <div className="w-full h-fit flex flex-col gap-10 border border-winter-400 p-10">
             <p className="text-white font-cinzel text-sm md:text-2xl tracking-wide leading-relaxed">
               Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro
               error tempora beatae cupiditate. Optio voluptatem, rerum
@@ -48,7 +48,7 @@ export default function Home() {
       </section>
 
       <section
-        className={`w-full h-screen  flex bg-[url(../src/assets/img/background/8DCE109A-2ECB-486B-9248-EED37BEB3469.jpg)]  bg-cover bg-center  bg-fixed`}
+        className={`w-full h-screen  flex bg-[url(../src/assets/img/background/8DCE109A-2ECB-486B-9248-EED37BEB3469.jpg)] bg-cover bg-center md:bg-fixed`}
       >
         <div className="absolute w-full h-screen bg-black bg-opacity-50 bg-fixed"></div>
         <div className="p-20 gap-10 flex flex-col items-center justify-center z-20">
@@ -65,7 +65,7 @@ export default function Home() {
       </section>
 
       <section
-        className={`w-full h-screen  flex bg-[url(../src/assets/img/background/_DSC0445.JPG)]  bg-cover bg-center  bg-fixed`}
+        className={`w-full h-screen  md:h-screen  flex bg-[url(../src/assets/img/background/_DSC0445.JPG)] bg-cover bg-center  md:bg-fixed`}
       >
         <div className="absolute w-full h-screen bg-black bg-opacity-50 bg-fixed"></div>
         <div className="p-20 gap-10 flex flex-col items-center justify-center z-20">
@@ -82,7 +82,7 @@ export default function Home() {
       </section>
 
       <section
-        className={`w-full h-screen  flex bg-[url(../src/assets/img/background/7FD1FBD9-BC6E-4ADA-B80B-33CB62F47775.jpg)]  bg-cover bg-center  bg-fixed`}
+        className={`w-full h-screen  flex bg-[url(../src/assets/img/background/7FD1FBD9-BC6E-4ADA-B80B-33CB62F47775.jpg)]  bg-cover bg-center  md:bg-fixed`}
       >
         <div className="absolute w-full h-screen bg-black bg-opacity-50 bg-fixed"></div>
         <div className="p-20 gap-10 flex flex-col items-center justify-center z-20">
@@ -122,11 +122,11 @@ export default function Home() {
           </div>
         </div>
 
-        <div className=" p-5   flex flex-col items-center justify-normal gap-3">
+        <div className="max-w-full md:p-5  flex flex-col items-center justify-normal gap-3">
           <h3 className="font-primary text-winter-200 text-xl">
             DEJAME TU MENSAJE
           </h3>
-          <form className="flex flex-col gap-2">
+          <form className="flex flex-col gap-2 w-full">
             <input
               className="p-2 bg-gray-700"
               type="text"
